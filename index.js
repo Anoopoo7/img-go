6e938ac9b6a089007dfc3f4de8f03d5331626e74
anoop007 <anoopviswanadh007@gmail.com>
2024-04-02 18:22:26 +0530

    feat: multi file uploads

--- index.js
+++ index.js
@@ -1,50 +1,70 @@
 const { v4 } = require("uuid");
 
-const imageUpload = require("../../config.json")
+const imageUpload = require("../../config.json");
+
+async function handleUpload(imageFile, format) {
+  const { imgConfig } = imageUpload || {};
+  try {
+    const unique_id = v4();
+
+    var myHeaders = new Headers();
+    myHeaders.append("Cookie", imgConfig.cookie);
+
+    var formdata = new FormData();
+    formdata.append("source", imageFile, unique_id + (format || ".jpg"));
+    formdata.append("type", imgConfig.fileType);
+    formdata.append("action", imgConfig.action);
+    formdata.append("timestamp", new Date().getTime());
+    formdata.append("auth_token", imgConfig.authToken);
+
+    const response = await fetch("https://imgbb.com/json", {
+      method: "POST",
+      headers: myHeaders,
+      body: formdata,
+      redirect: "follow",
+    });
+
+    const result = await response.json();
+
+    return {
+      image: result?.image?.url || result?.image?.display_url,
+      height: result?.image?.display_height,
+      width: result?.image?.display_width,
+      size: result?.image?.size_formatted,
+      delete: result?.image?.delete_url,
+    };
+  } catch (error) {
+    console.error("error", error);
+    return null;
+  }
+}
 
 async function upload(event, format) {
-    const { imgConfig } = imageUpload || {}
-    try {
-        const imageFile = event?.target?.files?.[0] || false;
-        const unique_id = v4();
+  const imageFile = event?.target?.files?.[0] || false;
 
-        if (!imageFile) {
-            console.log("Image Not Found??");
-            return null;
-        }
+  if (!imageFile) {
+    console.log("Image Not Found??");
+    return null;
+  }
+  return await handleUpload(imageFile, format);
+}
 
-        var myHeaders = new Headers();
-        myHeaders.append('Cookie', imgConfig.cookie);
+async function uploads(event, format) {
+  const imageFiles = event?.target?.files || [];
 
-        var formdata = new FormData();
-        formdata.append('source', imageFile, unique_id + format || '.jpg');
-        formdata.append('type', imgConfig.fileType);
-        formdata.append('action', imgConfig.action);
-        formdata.append('timestamp', new Date().getTime());
-        formdata.append('auth_token', imgConfig.authToken);
+  if (imageFiles.length <= 0) {
+    console.log("Images Not Found??");
+    return null;
+  }
 
-        const response = await fetch('https://imgbb.com/json', {
-            method: 'POST',
-            headers: myHeaders,
-            body: formdata,
-            redirect: 'follow',
-        });
+  const uploadPromises = imageFiles.map(async (imageFile) => {
+    return await handleUpload(imageFile, format);
+  });
 
-        const result = await response.json();
-
-        return ({
-            image: result?.image?.url || result?.image?.display_url,
-            height: result?.image?.display_height,
-            width: result?.image?.display_width,
-            size: result?.image?.size_formatted,
-            delete: result?.image?.delete_url
-        });
-    } catch (error) {
-        console.error('error', error);
-        return null;
-    }
+  return await Promise.all(uploadPromises);
 }
 
 module.exports = {
-    upload
-}
+  upload,
+  uploads,
+};
